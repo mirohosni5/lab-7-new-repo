325cf81b8d6b66b4f8413a6988b760746cf41060
Aya-Gebril <aya2gebril@gmail.com>
2025-11-24 00:45:03 +0200

    Certificate class

--- src/main/java/models/Certificate.java
+++ src/main/java/models/Certificate.java
@@ -1,4 +1,13 @@
 package models;
 
+import java.time.LocalDate;
+
 public class Certificate {
+    private String certificateId;
+    private String studentId;
+    private String courseId;
+    private LocalDate issueDate;
+    private String studentName;
+    private String courseName;
+
 }
