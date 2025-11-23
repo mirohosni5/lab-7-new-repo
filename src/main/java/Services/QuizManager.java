4d032d394b847a785a93f36cc28f759f561298c2
Aya-Gebril <aya2gebril@gmail.com>
2025-11-23 21:51:27 +0200

    QuizManager class attributes and const.

--- src/main/java/Services/QuizManager.java
+++ src/main/java/Services/QuizManager.java
@@ -3,4 +3,12 @@ package Services;
 import models.*;
 
 public class QuizManager {
+    private final JsonDatabaseManager db;
+    private final AnalyticsService analytics;
+
+    public QuizManager(JsonDatabaseManager db, AnalyticsService analytics) {
+        this.db = db;
+        this.analytics = analytics;
+    }
+
 }
